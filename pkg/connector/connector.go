9af7599fb2d5a4d59ea745b0c04d60e6d256df58
Lauren Leach <lauren.leach@conductorone.com>
2025-01-21 17:44:19 -0800

    check if resource tags is empty before unmarshalling

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -69,9 +69,12 @@ func (d *Connector) Validate(ctx context.Context) (annotations.Annotations, erro
 func New(ctx context.Context, config *Config) (*Connector, error) {
 	l := ctxzap.Extract(ctx)
 	var resourceTags []*client.ResourceTag
-	err := json.Unmarshal([]byte(config.ResourceTags), &resourceTags)
-	if err != nil {
-		return nil, resourceTagErr
+	
+	if config.ResourceTags != "" {
+		err := json.Unmarshal([]byte(config.ResourceTags), &resourceTags)
+		if err != nil {
+			return nil, resourceTagErr
+		}
 	}
 
 	for _, rt := range resourceTags {
